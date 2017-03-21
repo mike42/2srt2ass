06cc2344923de2f73a315ea3080abcd7f9a41c6e
Michael Billington <michael.billington@gmail.com>
2017-03-21 22:04:39 +1100

    [WIP] interface re-write

---
+++ Gruntfile.js
@@ -0,0 +1,94 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    copy: {
+      images: {
+        expand: true,
+        flatten: true,
+        src: [
+          'src/images/**/*.ico',
+          'src/images/**/*.png',
+          'src/images/**/*.svg',
+          'node_modules/bootstrap-colorpicker/dist/img/bootstrap-colorpicker/*.png'
+        ],
+        dest: 'public/images/'
+      },
+      js: {
+        expand: true,
+        flatten: true,
+        src: [
+          'node_modules/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js'
+        ],
+        dest: 'public/js/'
+      }
+    },
+    postcss: {
+      dist: {
+        src: 'public/css/site.css'
+      },
+      options: {
+        map: {
+          inline: false,
+          annotation: 'public/css'
+        },
+        processors: [
+          require('autoprefixer')({
+            browsers: 'last 2 versions'
+          }),
+          require('postcss-uncss')({
+            html: ['src/index.html'],
+            ignore: [/.*colorpicker.*/, '.dropdown-menu']
+          }),
+          require('cssnano')({
+            discardComments: {
+              removeAll: true
+            }
+          })
+        ]
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'public/css/site.css': 'src/style/main.scss'
+        },
+        options: {
+          loadPath: [
+            'node_modules/bootstrap/scss',
+            'node_modules/bootstrap-colorpicker/src/sass',
+          ]
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['src/style/*', 'public/index.html'],
+        tasks: ['css'],
+        options: {
+          livereload: true,
+        }
+      },
+      configFiles: {
+        files: ['Gruntfile.js'],
+        options: {
+          reload: true
+        }
+      }
+    }
+  });
+
+  /* Load modules */
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-systemjs-builder');
+  grunt.loadNpmTasks('grunt-uncss');
+
+  /* Configure tasks */
+  grunt.registerTask('js', ['copy:js']);
+  grunt.registerTask('css', ['sass:dist', /* 'postcss:dist' */]);
+  grunt.registerTask('default', ['js', 'css', 'copy:images']);
+};
--- public/index.php
+++ public/index.php
@@ -1,160 +1,234 @@
-<?php
-require_once(__DIR__ . '/../vendor/autoload.php');
+<!DOCTYPE html>
+<html lang="en">
 
-use CombineSubtitles\Web;
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Subtitle merge tool - 2srt2ssa</title>
 
-// Process page
-$converterWeb = new Web();
-$error = null;
-$formVals = $converterWeb -> processPage($_POST, $_FILES, $_COOKIE);
-if ($formVals === false) {
-    // A file has already been sent to the user
-    die();
-}
-if (isset($formVals['error'])) {
-    $error = $formVals['error'];
-}
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>2srt2ssa</title>
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="jscolor/jscolor.js"></script>
-        <link href="//fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet" type="text/css"></link>
+  <!-- Bootstrap -->
+  <link href="css/site.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="images/favicon32.ico" />
+  <link rel="icon" type="image/png" href="images/favicon32.png" />
 
-<script type="text/javascript">
-$(function() {
-    $('option').each(function(index, Element) {
-        $Element = $(Element);
-        $Element.text($Element.attr('value'));
-    });
-});
-</script>
-<style type="text/css">
-html { width: 100%; height: 100%; background: url("background.svg") no-repeat center center fixed; background-size: cover; }
-body { font-family: 'Noto Sans', sans-serif; margin: 0px; padding: 0px; color: #595959; }
-.page { width: 800px; margin: 25px auto; background-color: rgb(220, 220, 220); background-color: rgba(220, 220, 220, 0.75); }
-h1 { background-color: #595959; color: #999999; padding: 3px 15px; }
-h2 { background-color: #909090; color: #CCCCCC; padding: 3px 15px; }
-h3 { padding: 3px 15px; }
-p { padding: 0; margin: 20px 30px; }
-.error { margin: 20px; border: 1px solid #909090; }
-.error h2 { margin: 0; }
-.error p { font-weight: bold; margin: 10px 15px; color: #451C1C; }
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
 
-.donate { border: 1px solid grey; padding: 3px 12px; background-color: #EEEEEE; display: inline-block; border-radius: 15px; }
-.donate a { color: red; text-decoration: none; }
+<body>
+  <div class="container">
+    <div class="header clearfix">
+      <ul class="nav nav-pills float-right">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Web <span class="sr-only">(current)</span></a>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link" href="#cli">Command-line</a>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link" href="https://github.com/mike42/2srt2ssa">Code</a>
+        </li>
+      </ul>
+      <h2>Subtitle merge tool</h2>
+    </div>
 
-.main-form { padding: 0 0 0 30px; }
-.main-form form { }
-.main-form form .form-row { clear: both; margin: 0 0 7px 0; }
-.main-form form .form-row .form-label { width: 135px; float: left; }
-.main-form form .form-row .form-input { }
-.main-form form .form-row .form-input input { border: 1px solid #595959; }
-.main-form form .form-row .form-input input[type=file] { width: 600px; }
+    <div class="jumbotron">
 
-</style>
-    </head>
-    <body>
-        <div class="page">
-            <h1 class="main-title">2srt2ssa</h1>
-<?php if ($error != null) : ?>
-            <div class="error">
-                <h2>Error</h2>
-                <p><?= $error ?></p>
-            </div>
-<?php endif; ?>
-            <h3>Merge two .srt files, to one .ssa file</h3>
-            <div class="main-form">
-                <form action="." method="post" enctype="multipart/form-data" name="form">
-                    <input type="hidden" name="send" value="yes">
-                    
-                    <div class="form-row">
-                        <span class="form-label"><label for="top">Top subtitle</label> :</span>
-                        <span class="form-input"><input type="file" id="top" name="top" /></span>
-                    </div>
-                    
-                    <div class="form-row">
-                        <span class="form-label"><label for="bot">Bottom subtitle</label> :</span>
-                        <span class="form-input"><input type="file" id="bot" name="bot" /></span>
-                    </div>
-                    
-                    <div class="form-row">
-                        <span class="form-label"><label for="fontsize">Font size</label> :</span>
-                        <span class="form-input">
-                            <select id="fontsize" name="fontsize">
-<?php
-foreach (Web::FONT_SIZES as $fontSize) {
-    $selected = $fontSize === $formVals['fontsize'] ? " selected" : "";
-    echo "                            <option value=\"" . htmlspecialchars($fontSize) . "\"$selected></option>\n";
-}
-?>
-                            </select>
-                        </span>
-                    </div>
-                    
-                    <div class="form-row">
-                        <span class="form-label"><label for="fontname">Font name</label> :</span>
-                        <span class="form-input">
-                            <select id="fontname" name="fontname">
-<?php
-foreach (Web::FONT_NAMES as $fontName) {
-    $selected = $fontName === $formVals['fontname'] ? " selected" : "";
-    echo "                            <option value=\"" . htmlspecialchars($fontName) . "\"$selected></option>\n";
-}
-?>
-                            </select>
-                        </span>
-                    </div>
-                    
-                    <div class="form-row">
-                        <span class="form-label"><label for="topColor">Top color</label> :</span>
-                        <span class="form-input"><input type="text" id="topColor" name="topColor" class="color {hash:true}" value="<?php echo htmlspecialchars($formVals['topColor']); ?>" />
-                    </div>
-                    
-                    <div class="form-row">
-                        <span class="form-label"><label for="botColor">Bottom color</label> :</span>
-                        <span class="form-input"><input type="text" id="botColor" name="botColor" class="color {hash:true}" value="<?php echo htmlspecialchars($formVals['botColor']); ?>" />
-                    </div>
-                    
-                    <div class="form-row">
-                        <span class="form-label"><label for="forceBottom">Force bottom</label> :</span>
-                        <span class="form-input"><input type="checkbox" id="forceBottom" name="forceBottom" value="1" /><label for="forceBottom"> [EXPERIMENTAL]</label></span>
-                    </div>
-                                        
-                    <input type="submit" />
-                    
-                </form>
-            </div>
-            <div class="about">
-                <h2>About</h2>
-                <h3>How to use</h3>
-                <p>Upload 2 srt files, the script will send you a SSA/ASS subtitle files. This file will have on top the fisrt file's subtitles and on bottom those of the second file.</p>
-                <p>If you have any problem, make sure both your files are <a href="https://en.wikipedia.org/wiki/SubRip#SubRip_text_file_format">formatted in SubRip (srt)</a> and are using the same encoding (like UTF-8).</p>
-                <h3>Author</h3>
-                <p>2srt2ass developped by Vincent Rémond.</p>
-                <p>Feedback or bugs at <a href="mailto:vincent.remond@gmail.com?subject=Feedback%202SRT2ASS">vincent.remond@gmail.com</a>.</p>
-                <p>Program under GNU/GPL. <a href="https://github.com/vincentremond/2srt2ass/">Sources here</a></p>
-                <p class="donate"><a href="http://www.actionagainsthunger.org/take-action/donate">DONATE</a></p>
-                <h3>Changelog</h3>
-                <ul>
-                    <li><b>0.2.1</b> &ndash;  2017-01-22<br />- Added Helvetica &amp; minor change</li>
-                    <li><b>0.2.0</b> &ndash;  2016-09-27<br />- Added force subtitles to bottom</li>
-                    <li><b>0.1.1</b> &ndash;  2015-06-20<br />- Remove leading 0 in timestamps</li>
-                    <li><b>0.1.0</b> &ndash;  2013-04-07<br />- Improved UI<br />- Customizable font output</li>
-                    <li><b>0.0.2</b><br />- Fixed : Only Windows' style line returns for output file<br />- Added : Generated file's name based on bottom file<br />- Fixed : only display GIF files when downloading on Firefox</li>
-                    <li><b>0.0.1</b><br />- Fixed : debug remains<br />- Added : removal of HTML and position tags</li>
-                    <li><b>0.0.0</b><br />- First release</li>
-                </ul>
-            </div>
+      <p class="lead">This online tool allows you to merge two subtitle tracks.</p>
+
+      <div class="row text-center">
+        <div class="col-sm-3 offset-sm-2">
+          <div class="subtitle-box preview-together">
+            <div class="subtitle-spacer-top"></div>
+            <p class="subtitle subtitle-alt"></p>
+            <p class="subtitle subtitle-main">Hello</p>
+          </div>
         </div>
-<?php
-$gaPath = $_SERVER["DOCUMENT_ROOT"] . '/include/google-analytics.php';
-if (file_exists($gaPath)) {
-    include($gaPath);
-}
-?>
+        <div class="col-sm-2">
+          <p class="big-char">+</p>
+        </div>
+        <div class="col-sm-3">
+          <div class="subtitle-box preview-together">
+            <div class="subtitle-spacer-top"></div>
+            <p class="subtitle subtitle-alt"></p>
+            <p class="subtitle subtitle-main">Bonjour</p>
+          </div>
+        </div>
+      </div>
+
+      <p class="lead">You will need your input tracks to be in <a href="https://en.wikipedia.org/wiki/SubRip">SubRip (SRT)</a> or
+        <a href="https://en.wikipedia.org/wiki/WebVTT">WebVTT</a> format. The output contains both tracks in <a href="">SubStation Alpha (SSA)</a> format.</p>
+
+      <div class="subtitle-box preview-together" id="preview-box">
+        <div class="subtitle-spacer-top"></div>
+        <p class="subtitle subtitle-alt" id="preview-box-alt">Hello</p>
+        <div class="subtitle-spacer-middle"></div>
+        <p class="subtitle subtitle-main" id="preview-box-main">Bonjour</p>
+      </div>
+
+    </div>
+
+
+    <form>
+
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="card">
+            <div class="card-block">
+              <h4 class="card-title">Main track</h4>
+              <h6 class="card-subtitle mb-2 text-muted">This subtitle track will appear at the bottom of the display.</h6>
+
+              <div class="form-group">
+                <label for="exampleInputFile">Subtitle file</label>
+                <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+                <small id="fileHelp" class="form-text text-muted">File must be in WebVTT or SRT format.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputFile">Display color</label>
+                <div id="cp3" class="input-group colorpicker-component">
+                  <input id="cp3-input" type="text" value="#FFFFFF" class="form-control" />
+                  <span class="input-group-addon"><i></i></span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-6">
+          <div class="card">
+            <div class="card-block">
+              <h4 class="card-title">Alternative track</h4>
+              <h6 class="card-subtitle mb-2 text-muted">This subtitle track will be displayed above the main track.</h6>
+              <div class="form-group">
+                <label for="exampleInputFile">Subtitle file</label>
+                <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+                <small id="fileHelp" class="form-text text-muted">File must be in WebVTT or SRT format.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputFile">Display color</label>
+                <div id="cp4" class="input-group colorpicker-component">
+                  <input id="cp4-input" type="text" value="#FFFF00" class="form-control" />
+                  <span class="input-group-addon"><i></i></span>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <h4>Output options</h4>
+      <div class="form-group row">
+        <label for="inputEmail3" class="col-sm-3 col-form-label">Display font</label>
+        <div class="col-sm-9">
+          <select class="form-control custom-select">
+            <option selected>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="inputPassword3" class="col-sm-3 col-form-label">Text size</label>
+        <div class="col-sm-9">
+          <select class="form-control custom-select">
+            <option selected>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label class="col-sm-3">Vertical alignment</label>
+        <div class="col-sm-9">
+          <div class="custom-controls-stacked">
+            <label class="custom-control custom-radio">
+              <input name="radio-stacked" type="radio" id="alignTogetherBox" checked class="custom-control-input">
+              <span class="custom-control-indicator"></span>
+              <span class="custom-control-description">Both subtitles together at the bottom of the screen</span>
+            </label>
+            <label class="custom-control custom-radio">
+              <input name="radio-stacked" type="radio" id="alignApartBox" class="custom-control-input">
+              <span class="custom-control-indicator"></span>
+              <span class="custom-control-description">Main track at the bottom, alternate at the top,</span>
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="offset-sm-3 col-sm-9">
+          <button type="submit" class="btn btn-primary">Merge these files</button>
+        </div>
+      </div>
+    </form>
+
+    <footer class="footer">
+      <p><a href="https://github.com/mike42/2srt2ssa">2srt2ssa</a> is distributed under the terms of the GNU General Public License, version 3.</p>
+      <p>Your use of this website is entirely at your own risk. No warranty is provided by any party, implied or otherwise.</p>
+    </footer>
+
+  </div>
+
+
+
+
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <!-- <script src="js/site.min.js"></script> -->
+  <script src="js/bootstrap-colorpicker.min.js"></script>
+  <script>
+    var colorSelectors = {
+      'black': '#000000',
+      'white': '#ffffff',
+      'red': '#FF0000',
+      'default': '#777777',
+      'primary': '#337ab7',
+      'success': '#5cb85c',
+      'info': '#5bc0de',
+      'warning': '#f0ad4e',
+      'danger': '#d9534f'
+    };
+    $(function() {
+      $('#cp3').colorpicker({
+        format: 'hex',
+        transparent: true,
+        colorSelectors: colorSelectors
+      }).on('changeColor', function(ev) {
+        if(ev.value !== undefined) {
+          $('@preview-box-main').css('color', ev.value);
+        }
+      });
+      $('#cp4').colorpicker({
+        format: 'hex',
+        transparent: true,
+        colorSelectors: colorSelectors
+      }).on('changeColor', function(ev) {
+        if(ev.value !== undefined) {
+          $('#preview-box-alt').css('color', ev.value);
+        }
+      });
+
+      $('#preview-box-main').css('color', $('#cp3-input').val())
+      $('#preview-box-alt').css('color', $('#cp4-input').val());
+      $('#alignTogetherBox').on('change', function() {
+        $('#preview-box').addClass('preview-together');
+      });
+      $('#alignApartBox').on('change', function() {
+        $('#preview-box').removeClass('preview-together');
+      });
+
+    });
+  </script>
 </body>
+
 </html>
